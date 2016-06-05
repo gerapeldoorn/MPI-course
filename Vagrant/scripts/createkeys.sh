352487080e7be4dea40ddbcf74334413b1177e17
Ger Apeldoorn <info@gerapeldoorn.nl>
2016-06-05 10:24:34 +0200

    Fixed Vagrantfile

--- Vagrant/scripts/createkeys.sh
+++
@@ -1,6 +0,0 @@
-rsync -r /vagrant/keys /tmp/
-puppet agent -t --certdir=/tmp/keys/certs/ --privatekeydir=/tmp/keys/private/ --server demo.puppetspecialist.nl --detailed-exitcodes --environment=ger --noop
-echo "Sign cert and press key"
-read text
-puppet agent -t --certdir=/tmp/keys/certs/ --privatekeydir=/tmp/keys/private/ --server demo.puppetspecialist.nl --detailed-exitcodes --environment=ger --noop
-rsync -r /tmp/keys/* /vagrant/keys/
