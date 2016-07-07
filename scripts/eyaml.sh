56c2e75cab6b3fa97e4b7aae81093fb6f1124871
Ger Apeldoorn <info@gerapeldoorn.nl>
2016-07-07 13:27:58 +0200

    Create eyaml.sh

---
+++ scripts/eyaml.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+alias evim='/opt/puppetlabs/puppet/bin/eyaml edit --pkcs7-public-key /etc/puppetlabs/puppet/keys/public_key.pkcs7.pem --pkcs7-private-key /etc/puppetlabs/puppet/keys/private_key.pkcs7.pem '
